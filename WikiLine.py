cf31d91adf763b47d28677e0946750f65883a360
Mick Halsband <mick.halsband@gmail.com>
2013-03-21 23:09:52 +0200

    Update debug code. Parsed title

--- WikiLine.py
+++ WikiLine.py
@@ -1,34 +1,42 @@
 #import os
+import sys
 import urllib2
 from xml.dom import minidom
 
+product_mode="release"
 def debug(str):
-    print "***" + str
+    if product_mode == "debug":
+        print "***" + str
 
 class WikiLine:
     def __init__(self):
         self.opener = urllib2.build_opener()
-        self.opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        self.opener.addheaders = [("User-agent", "Mozilla/5.0")]
 
-    def run(self):
+    def run(self, url):
         print "Running WikiLine..."
-        xmldoc = self.read_article("http://en.wikipedia.org/w/index.php?title=Albert_Einstein&printable=yes")
+        xmldoc = self.read_article(url)
         self.parse_article(xmldoc)
 
     def read_article(self, url):
+        print "reading url '%s'" %url
         infile = self.opener.open(url)
         page = infile.read()
-        print "***page length: " + str(page.__len__())
-        print "***page beings with: " + page[0:256]
+        debug ("page length: " + str(page.__len__()))
+        debug ("page beings with: " + page[0:256])
         xmldoc = minidom.parseString(page)
         #print ("xml length: " + str(xmldoc.__len__()))
         infile.close()
         return xmldoc
 
     def parse_article(self, xmldoc):
+        print "parsing XML"
         debug("parse_article(" + str(self) + "," + str(xmldoc) + ")")
-        title = xmldoc.getElementsByTagName('title')
-
+        for title in xmldoc.getElementsByTagName("title"):
+            print "title is: " + title.firstChild.nodeValue
+        
+        return
+        
         for e in title:
             print e
             #print e.Value
@@ -38,22 +46,31 @@ class WikiLine:
             else:
                 print "no attributes for " + str(e)
 
-        #debug("title is '" + title[0].attributes['name'].value + "'")
+        #debug("title is "" + title[0].attributes["name"].value + """)
 
-        tag = 'th'
+        tag = "th"
         itemlist = xmldoc.getElementsByTagName(tag)
         debug ("itemlist length: " + str(len(itemlist)))
                     
-        debug ("iterating elements by tag '" + tag + "'")
+        debug ("iterating elements by tag "" + tag + """)
         for e in itemlist[0:1] :
             print "element is: " + str(e)
             print "element attributes: " + str(e.attributes)
-#          print s.attributes['name'].value
+#          print s.attributes["name"].value
 
         #print xmldoc.toxml()
 
-#this calls the 'main' function when this script is executed
-if __name__ == '__main__':
+#this calls the "main" function when this script is executed
+#"http://en.wikipedia.org/w/index.php?title=Albert_Einstein&printable=yes"
+if __name__ == "__main__":
+    url = "http://en.wikipedia.org/w/index.php?title=Albert_Einstein&printable=yes"
+    if len(sys.argv) < 2:
+        sys.exit("Usage: %s [url]" % sys.argv[0])
+
+    url = sys.argv[1]
+    
     print "Starting world..."
+
+    print "     arg[1] = %s" %sys.argv[1]
     wikiLine = WikiLine()
-    wikiLine.run()
+    wikiLine.run(url)
