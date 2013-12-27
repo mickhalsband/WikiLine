5699e7750aafc0d7d6293ff2f5cc36796bb9b2d0
Mick Halsband <mick.halsband@gmail.com>
2013-12-27 19:00:51 +0200

    Upgrade GUI to a controller (FloatLayout) with a working button

--- main.py
+++ main.py
@@ -4,33 +4,55 @@ import wiki_line
 from subprocess import call
 import sys
 import kivy
+
 kivy.require('1.0.7')
 
+from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
 
 
-class TestApp(App):
-    url = ""
+class Controller(FloatLayout):
     """
     this calls the "main" function when this script is executed
     "http://en.wikipedia.org/w/index.php?title=Albert_Einstein&printable=yes"
+    Create a controller that receives a custom widget from the kv lang file.
+
+    Add an action to be called from the kv lang file.
     """
 
+    url = ""
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
     def __init__(self, url, **kwargs):
-        super(TestApp, self).__init__(**kwargs)
+        super(Controller, self).__init__(**kwargs)
         self.url = url
 
+    def do_action(self):
+        self.label_wid.text = 'running wikiline'
+        self.info = 'New info text'
+        self.do_stuff()
+
     def do_stuff(self):
-        # print "Starting world..."
+        print "Starting world..."
 
-        # wikiLine = wiki_line.WikiLine()
-        # wikiLine.run(url)
-        #
-        # call(["python", "/Users/mick/Development/externals/timeline-0.19.0/timeline.py", "/Users/mick/timeline_test.timeline"])
-        print 'now in do stuff'
-        pass
+        wikiLine = wiki_line.WikiLine()
+        wikiLine.run(self.url)
 
-if __name__ == '__main__':
+        call(["python", "/Users/mick/Development/externals/timeline-0.19.0/timeline.py", "/Users/mick/timeline_test.timeline"])
+        print 'done'
+
+class ControllerApp(App):
+    def __init__(self, url, **kwargs):
+        super(ControllerApp, self).__init__(**kwargs)
+        self.url = url
+
+    def build(self):
+        return Controller(self.url)
+
+
+def main():
     url = "http://en.wikipedia.org/w/index.php?title=Albert_Einstein"
     if len(sys.argv) < 2:
         print "Usage: %s [url]" % sys.argv[0]
@@ -38,4 +60,8 @@ if __name__ == '__main__':
         print "arg[1] = %s" % sys.argv[1]
         url = sys.argv[1]
 
-    TestApp(url).run()
+    ControllerApp(url).run()
+
+
+if __name__ == '__main__':
+    main()
